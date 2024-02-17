12e1d2ccfcfe0b58c079601ba97e5071ad8af335
Shaker Ahamed <ahamedshaker7@gmail..com>
2024-02-17 11:59:29 +0600

    New changes added

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router-dom"; 
-import React from "react";  
+import React from "react"; 
+import Calculator from "./component/Calculator";
 import StopWatch from "./component/StopWatch";
  
-const routes = createBrowserRouter([ 
+const routes = createBrowserRouter([
+  
   {
     path:"/",
+    element: <Calculator/>
+  },
+  {
+    path:"/time-watch",
     element: <StopWatch/>
   }
 ]);
